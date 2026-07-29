1453b2e8392a86c539487b4cbb3aa6cbc4fdde33
Lishani-Samarakoon <lishanisamarakoon@gmail.com>
2026-07-29 14:40:53 +0530

    Add data loading and cleaning functions

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,1 +1,11 @@
+# Preprocessing script
 
+import pandas as pd
+
+def load_data(path):
+    return pd.read_csv(path)
+
+def clean_data(df):
+    df = df.drop_duplicates()
+    df = df.dropna()
+    return df
